c3a933cf725318de860b0291fab76ee567edf66b
이샛별 <koalalovepabro@gmail.com>
2023-04-06 01:59:15 +0900

    ~ #7.3 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    const [budget, setBudget] = useState('');
+
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, [])
+    // console.log(coins)
+
+    const onChange = (event) => {
+        setBudget(event.target.value);
+    }
+
+    return (
+        <div>
+            <h1>The Coins !
+                {loading
+                    ? ''
+                    : `(${coins.length})`}
+            </h1>
+            <input value={budget}
+                   onChange={onChange}
+                   type="text"
+                   placeholder="How much do you have?"/><br></br>
+
+            {loading
+                ? ""
+                : (<select>
+                    {coins.map((coin) => (
+                        <option> {coin.name} ({coin.symbol}) : You can
+                            buy {budget / coin.quotes.USD.price} {coin.symbol} with
+                            your money.
+                        </option>
+                    ))}
+                </select>)
+            }
+        </div>
+    )
+};
 
 export default App;
