cd934aff13e30cd046a2c5be12b85435520f4895
이샛별 <koalalovepabro@gmail.com>
2023-04-11 00:56:12 +0900

    #7.3 Movie App part One - 사진 불러오기

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 
     const getMovies = async () => {
         const json = await (
+            // 평점 8.8 이상의 영화만 가져오기 -> minimum_rating=8.8
             await fetch(
                 `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
         ).json();
@@ -24,6 +25,7 @@ function App() {
             ? <h1>Loading.....</h1>
             : movies.map((movie) =>
                 <div key = {movie.index}>
+                    <img src = {movie.medium_cover_image} />
                     <h2>{movie.title}</h2>
                     <p>{movie.summary}</p>
                     <ul>
