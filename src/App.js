c13858098c9d249933a6f605ce6d1adfef8d9e33
이샛별 <koalalovepabro@gmail.com>
2023-04-17 04:01:49 +0900

    try to release

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path = '/hi' element={<h1>Hi there !</h1>}/>
-                <Route path = '/' element={<Home />}/>
-                <Route path = '/movie/:tomato' element={<Detail />}/>
+                {/*<Route path = '/hi' element={<h1>Hi there !</h1>}/>*/}
+                <Route path = {`${process.env.PUBLIC_URL}/`}element={<Home />}/>
+                <Route path = {`${process.env.PUBLIC_URL}/movie/:tomato`} element={<Detail />}/>
             </Routes>
         </BrowserRouter>
     )
