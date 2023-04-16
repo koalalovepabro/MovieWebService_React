f335dd6119bf62127ffed23783cd2ef7dd665ba3
이샛별 <koalalovepabro@gmail.com>
2023-04-16 17:57:01 +0900

    #7.5 React Router - Link

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import { useState, useEffect } from "react";
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Movie from "./Movie";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path = '/' element={<Home />}/>
+                <Route path = '/movie' element={<Detail />}/>
 
-    const getMovies = async () => {
-        const json = await (
-            // 평점 8.8 이상의 영화만 가져오기 -> minimum_rating=8.8
-            await fetch(
-                `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-        ).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        getMovies();
-    }, [])
-    console.log(movies)
-
-    return <div>
-        {loading
-            ? <h1>Loading.....</h1>
-            : movies.map((movie) =>
-                <div key = {movie.index}>
-                    <img src = {movie.medium_cover_image} />
-                    <h2>{movie.title}</h2>
-                    <p>{movie.summary}</p>
-                    <ul>
-                        {movie.genres.map((g, index) => <li key = {index}>{g}</li>)}
-                    </ul>
-                </div>)
-        } </div>;
+            </Routes>
+        </BrowserRouter>
+    )
 };
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import Movie from "../Movie";
+
+function Home() {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+
+    const getMovies = async () => {
+        const json = await (// 평점 8.8 이상의 영화만 가져오기 -> minimum_rating=8.8
+            await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)).json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, [])
+    console.log(movies)
+
+    return (<div>
+            {loading ? <h1>Loading.....</h1> : (<div>
+                    {movies.map((movie) => (<Movie
+                            coverImg={movie.medium_cover_image}
+                            title={movie.title}
+                            summary={movie.summary}
+                            genres={movie.genres}
+
+                        />))}
+                </div>
+
+            )}
+        </div>);
+}
+
+export default Home;
