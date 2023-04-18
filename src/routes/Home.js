f840e02e718c015e347a49d200ea333dbdf8e378
이샛별 <koalalovepabro@gmail.com>
2023-04-18 17:07:28 +0900

    style 적용 (Home.js)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
-import Movie from "../Movie";
+import {useEffect, useState} from "react";
+import Movie from "../components/Movie";
+import styles from "./Home.module.css";
+
 
 function Home() {
     const [loading, setLoading] = useState(true);
@@ -17,19 +19,26 @@ function Home() {
     }, [])
     // console.log(movies)
 
-    return (<div>
-            {loading ? <h1>Loading.....</h1> : (<div>
-                    {movies.map((movie) => (<Movie
+    return (
+        <div className={styles.container}>
+            {loading
+                ? (
+                    <div className={styles.loader}>
+                        <h1>Loading.....</h1>
+                    </div>)
+                : (
+                    <div className={styles.movies}>
+                        {movies.map((movie) => (<Movie
                             coverImg={movie.medium_cover_image}
                             title={movie.title}
                             summary={movie.summary}
                             genres={movie.genres}
-                            key = {movie.id}
-                            id = {movie.id}
+                            key={movie.id}
+                            id={movie.id}
                         />))}
-                </div>
+                    </div>
 
-            )}
+                )}
         </div>);
 }
 
